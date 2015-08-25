4a685382def338b57d9c9626f8ed12abb8b02d3a
Owen Jow <owenjow@berkeley.edu>
2015-08-25 01:38:44 -0700

    Developed a loading screen!

--- app/src/main/java/xyz/owenjow/swa/StartsWithAnActivity.java
+++ app/src/main/java/xyz/owenjow/swa/StartsWithAnActivity.java
@@ -1,6 +1,8 @@
 package xyz.owenjow.swa;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,12 +18,15 @@ import java.util.HashMap;
  * @author Owen Jow
  */
 public class StartsWithAnActivity extends AppCompatActivity {
-    Spinner letterSpinner;
-    NounDatabase nounDB;
+    private Spinner letterSpinner;
+    private NounDatabase nounDB;
+    private Dialog loadingDialog;
+    private static final int CHECK_DELAY = 1000; // ms between processing completion checks
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        displayLoadingScreen();
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_starts_with_an);
@@ -30,8 +35,13 @@ public class StartsWithAnActivity extends AppCompatActivity {
         letterSpinner = (Spinner) findViewById(R.id.letterSpinner);
         configureSpinner();
 
-        // Process all of the noun data
-        nounDB = new NounDatabase(getAssets());
+        // WordNet parsing performed in a separate thread so as not to bottleneck everything
+        new Thread(new Runnable() {
+            public void run() {
+                // Process all of the noun data
+                nounDB = new NounDatabase(getAssets());
+            }
+        }).start();
     }
 
     /**
@@ -42,6 +52,41 @@ public class StartsWithAnActivity extends AppCompatActivity {
                 R.array.letters, R.layout.spinner_item_view);
         adapter.setDropDownViewResource(R.layout.spinner_item_view);
         letterSpinner.setAdapter(adapter);
+    }
+
+    /**
+     * Shows the loading screen (to be used while the noun data is being processed).
+     */
+    private void displayLoadingScreen() {
+        loadingDialog = new Dialog(this, R.style.LoadingScreen);
+        loadingDialog.setContentView(R.layout.loading_screen);
+        loadingDialog.setCancelable(false);
+        loadingDialog.show();
+
+        // Of course, we'll want to remove the loading screen after the background job is done
+        activateCheckHandler();
+    }
+
+    /**
+     * Schedules a processing completion check for the near future.
+     * For each check: if the noun processing has finished, we will close
+     * the loading dialog so that the main content can be displayed. Otherwise
+     * we will set another check to run in CHECK_DELAY ms.
+     */
+    private void activateCheckHandler() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (nounDB != null) {
+                    // Processing is complete; remove the loading screen
+                    loadingDialog.dismiss();
+                    loadingDialog = null;
+                } else {
+                    // Check again in CHECK_DELAY ms
+                    activateCheckHandler();
+                }
+            }
+        }, CHECK_DELAY);
     }
 
     /**
